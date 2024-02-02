916c6845bc26e3c8959d924339f37a65737a0b63
DaylightNebula <35437205+DaylightNebula@users.noreply.github.com>
2024-02-01 23:01:00 -0700

    reverting some stuff

--- examples/test_components.rs
+++ examples/test_components.rs
@@ -1,5 +1,5 @@
 use cgmath::Quaternion;
-use forte_engine::{component_app::EngineComponent, math::{quaternion::QuaternionExt, transforms::Transform}, primitives::{cameras::Camera, mesh::Mesh, textures::Texture, transforms::TransformRaw, vertices::Vertex}, render::pipelines::Pipeline, run_app, utils::resources::Handle};
+use forte_engine::{component_app::EngineComponent, create_app, math::{quaternion::QuaternionExt, transforms::Transform}, primitives::{cameras::Camera, mesh::Mesh, textures::Texture, transforms::TransformRaw, vertices::Vertex}, render::pipelines::Pipeline, run_app, utils::resources::Handle};
 
 const VERTICES: &[Vertex] = &[
     Vertex { position: [ 0.5, -0.5, -0.5], tex_coords: [0.4131759, 0.00759614], normal: [0.0, 0.0, 0.0] },
@@ -37,71 +37,71 @@ pub struct TestComponent {
     instance_buffer: wgpu::Buffer
 }
 
-// impl EngineComponent<App> for TestComponent {
-//     fn create(engine: &mut RenderEngine) -> Self {
-//         // create render pipeline
-//         let pipeline = Pipeline::new(
-//             "std", &engine, include_str!("rotating_cube.wgsl"),
-//             &[Vertex::desc(), TransformRaw::desc()],
-//             &[
-//                 &engine.device.create_bind_group_layout(&Camera::BIND_LAYOUT),
-//                 &engine.device.create_bind_group_layout(&Texture::BIND_LAYOUT),
-//             ]
-//         );
+impl EngineComponent<App> for TestComponent {
+    fn create(engine: &mut RenderEngine) -> Self {
+        // create render pipeline
+        let pipeline = Pipeline::new(
+            "std", &engine, include_str!("rotating_cube.wgsl"),
+            &[Vertex::desc(), TransformRaw::desc()],
+            &[
+                &engine.device.create_bind_group_layout(&Camera::BIND_LAYOUT),
+                &engine.device.create_bind_group_layout(&Texture::BIND_LAYOUT),
+            ]
+        );
 
-//         // generate camera
-//         let mut camera = Camera::new(
-//             &engine, 
-//             engine.config.width as f32 / engine.config.height as f32,
-//             45.0, 0.1, 100.0
-//         );
-//         camera.position = (0.0, 0.0, 5.0).into();
-//         camera.update(engine);
+        // generate camera
+        let mut camera = Camera::new(
+            &engine, 
+            engine.config.width as f32 / engine.config.height as f32,
+            45.0, 0.1, 100.0
+        );
+        camera.position = (0.0, 0.0, 5.0).into();
+        camera.update(engine);
 
-//         // create instances
-//         let instances = vec![Transform {
-//             position: cgmath::Vector3 { x: 0.0, y: 0.0, z: 0.0 },
-//             rotation: cgmath::Quaternion::euler_deg_z(0.0),
-//             scale: (1.0, 1.0, 1.0).into()
-//         }];
+        // create instances
+        let instances = vec![Transform {
+            position: cgmath::Vector3 { x: 0.0, y: 0.0, z: 0.0 },
+            rotation: cgmath::Quaternion::euler_deg_z(0.0),
+            scale: (1.0, 1.0, 1.0).into()
+        }];
 
-//         Self {
-//             mesh: engine.create_mesh("test", VERTICES, INDICES), 
-//             texture: engine.create_texture("test", include_bytes!("rotating_cube.png")),
-//             instance_buffer: TransformRaw::buffer_from_generic(engine, &instances),
-//             instances, camera, pipeline
-//         }
-//     }
+        Self {
+            mesh: engine.create_mesh("test", VERTICES, INDICES), 
+            texture: engine.create_texture("test", include_bytes!("rotating_cube.png")),
+            instance_buffer: TransformRaw::buffer_from_generic(engine, &instances),
+            instances, camera, pipeline
+        }
+    }
 
-//     fn start(_: &mut App) {}
+    fn start(_: &mut App) {}
 
-//     fn update(components: &mut App) {
-//         components.test.camera.update(&mut components.render_engine);
-//         TransformRaw::update_buffer_generic(&components.render_engine, &components.test.instance_buffer, &[
-//             Transform {
-//                 rotation: Quaternion::euler_deg(0.0, components.render_engine.time_since_start * 45.0, components.render_engine.time_since_start * 45.0),
-//                 ..Default::default()
-//             }
-//         ]);
-//     }
+    fn update(components: &mut App) {
+        components.test.camera.update(&mut components.render_engine);
+        TransformRaw::update_buffer_generic(&components.render_engine, &components.test.instance_buffer, &[
+            Transform {
+                rotation: Quaternion::euler_deg(0.0, components.render_engine.time_since_start * 45.0, components.render_engine.time_since_start * 45.0),
+                ..Default::default()
+            }
+        ]);
+    }
 
-//     fn render<'rpass>(&'rpass self, render_engine: &'rpass RenderEngine, pass: &mut wgpu::RenderPass<'rpass>) {
-//         self.pipeline.bind(pass);
-//         self.camera.bind(pass, 0);
-//         render_engine.draw_textured_mesh(pass, &self.mesh, &self.texture, &self.instance_buffer, self.instances.len() as u32);
-//     }
+    fn render<'rpass>(&'rpass self, render_engine: &'rpass RenderEngine, pass: &mut wgpu::RenderPass<'rpass>) {
+        self.pipeline.bind(pass);
+        self.camera.bind(pass, 0);
+        render_engine.draw_textured_mesh(pass, &self.mesh, &self.texture, &self.instance_buffer, self.instances.len() as u32);
+    }
 
-//     fn exit(_: &mut App) {}
-// }
+    fn exit(_: &mut App) {}
+}
 
-// create_app!(
-//     COMPONENTS => [
-//         test => TestComponent
-//     ]
+create_app!(
+    COMPONENTS => [
+        test => TestComponent
+    ]
 
-//     PASSES => [
-//         0 => [test]
-//     ]
-// );
+    PASSES => [
+        0 => [test]
+    ]
+);
 
-fn main() { /* run_app::<App>() */ }
+fn main() { run_app::<App>() }
--- src/component_app.rs
+++ src/component_app.rs
@@ -1,100 +1,75 @@
-use fxhash::FxHashMap;
-
 use crate::render::render_engine::RenderEngine;
 
-pub trait EngineComponentFactory {
-    fn id() -> u128;
-    fn create(engine: &mut App) -> Self;
-}
-
-pub trait EngineComponent {
-    fn start(&mut self, components: &mut App);
-    fn update(&mut self, components: &mut App);
+pub trait EngineComponent<C> {
+    fn create(engine: &mut RenderEngine) -> Self;
+    fn start(components: &mut C);
+    fn update(components: &mut C);
     fn render<'rpass>(&'rpass self, render_engine: &'rpass RenderEngine, pass: &mut wgpu::RenderPass<'rpass>);
-    fn exit(&mut self, component: &mut App);
+    fn exit(component: &mut C);
 }
 
-pub struct App {
-    render_engine: RenderEngine,
-    components: FxHashMap<u128, Box<dyn EngineComponent>>
-}
+#[macro_export]
+macro_rules! create_app {
+    (
+        COMPONENTS => [$(
+            $component:ident => $type:ty
+        ),*]
+        PASSES => [$(
+            $pass_idx:literal => [$($to_render:ident),*]
+        ),*]
+    ) => {
+        use forte_engine::{EngineApp, start_render, end_render, pass, render::{input::EngineInput, render_engine::RenderEngine}};
 
-impl App {
-    // quick getters
-    pub fn render_engine(&self) -> &RenderEngine { &self.render_engine }
-    pub fn render_engine_mjt(&mut self) -> &mut RenderEngine { &mut self.render_engine }
-    
-    // component manipulation
-    pub fn get_component(&self, id: u128) -> Option<&Box<dyn EngineComponent>> { self.components.get(&id) }
-    pub fn get_component_mut(&mut self, id: u128) -> Option<&mut Box<dyn EngineComponent>> { self.components.get_mut(&id) }
-    pub fn add_component<C: EngineComponent + EngineComponentFactory + 'static>(&mut self) {
-        let component = C::create(self);
-        self.components.insert(C::id(), Box::new(component));
-    }
-}
+        pub struct App {
+            render_engine: RenderEngine,
+            $($component: $type,)*
+        }
 
-// #[macro_export]
-// macro_rules! create_app {
-//     (
-//         COMPONENTS => [$(
-//             $component:ident => $type:ty
-//         ),*]
-//         PASSES => [$(
-//             $pass_idx:literal => [$($to_render:ident),*]
-//         ),*]
-//     ) => {
-//         use forte_engine::{EngineApp, start_render, end_render, pass, render::{input::EngineInput, render_engine::RenderEngine}};
+        impl EngineApp for App {
+            fn create(mut render_engine: RenderEngine) -> Self {
+                $(let $component = <$type>::create(&mut render_engine);)*
+                Self {
+                    render_engine,
+                    $($component,)*
+                }
+            }
 
-//         pub struct App {
-//             render_engine: RenderEngine,
-//             $($component: $type,)*
-//         }
+            fn start(&mut self) {
+                $(
+                    <$type>::start(self);
+                )*
+            }
 
-//         impl EngineApp for App {
-//             fn create(mut render_engine: RenderEngine) -> Self {
-//                 $(let $component = <$type>::create(&mut render_engine);)*
-//                 Self {
-//                     render_engine,
-//                     $($component,)*
-//                 }
-//             }
+            fn update(&mut self) {
+                $(
+                    <$type>::update(self);
+                )*
 
-//             fn start(&mut self) {
-//                 $(
-//                     <$type>::start(self);
-//                 )*
-//             }
+                let mut resources = start_render!(self.render_engine);
+                $(
+                    {
+                        let pass_id = $pass_idx;
+                        let mut pass = pass!(self.render_engine, resources);
+                        $(
+                            self.$to_render.render(&self.render_engine, &mut pass);
+                        )*
+                        pass;
+                    }
+                )*
+                end_render!(self.render_engine, resources);
 
-//             fn update(&mut self) {
-//                 $(
-//                     <$type>::update(self);
-//                 )*
+                self.render_engine.next_frame();
+            }
 
-//                 let mut resources = start_render!(self.render_engine);
-//                 $(
-//                     {
-//                         let pass_id = $pass_idx;
-//                         let mut pass = pass!(self.render_engine, resources);
-//                         $(
-//                             self.$to_render.render(&self.render_engine, &mut pass);
-//                         )*
-//                         pass;
-//                     }
-//                 )*
-//                 end_render!(self.render_engine, resources);
-
-//                 self.render_engine.next_frame();
-//             }
-
-//             fn input(&mut self, input: EngineInput) {}
+            fn input(&mut self, input: EngineInput) {}
             
-//             fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) { self.render_engine.resize(new_size); }
+            fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) { self.render_engine.resize(new_size); }
 
-//             fn exit(&mut self) {
-//                 $(
-//                     <$type>::exit(self);
-//                 )*
-//             }
-//         }
-//     };
-// }
+            fn exit(&mut self) {
+                $(
+                    <$type>::exit(self);
+                )*
+            }
+        }
+    };
+}
